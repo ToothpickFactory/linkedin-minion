5732e3b9da7457cfe430554f656103984b2a94d6
Austin Lovell <austin.d.lovell@gmail.com>
2017-04-27 23:21:27 -0600

    cleaned up script

---
+++ linkedin-minion.jquery.js
@@ -0,0 +1,17 @@
+var peopleToConnect = 20;
+var scrollLoop = setInterval(function(){
+  var connectArr = $('button[data-control-name="invite"]');
+  if(connectArr.length >= peopleToConnect){
+    clearInterval(scrollLoop);
+    var index = 0;
+    var connectLoop = setInterval(function(){
+      connectArr[index].click();
+      index++;
+      if(index >= connectArr.length){
+        clearInterval(connectLoop);
+        alert("Done Connecting!");
+      }
+    }, 1500);
+  }
+  window.scrollTo(0, document.body.scrollHeight);
+}, 500);
